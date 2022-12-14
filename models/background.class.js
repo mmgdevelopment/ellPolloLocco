ac6e3bc9f0d31f3fa87b2fc3b9b0b461e1c59483
Marcel Gregor <marcel_gregor@icloud.com>
2022-12-14 20:27:19 +0100

    drawable Objects added to canvas

--- models/background.class.js
+++ models/background.class.js
@@ -4,8 +4,8 @@ class Background extends MovableObject {
         super().loadImage(path)
         this.x = 0;
         this.y = 0;
-        this.width = 720;
-        this.height = 480;
+        this.width = 720 * 1.3;
+        this.height = 480 * 1.3;
     }
 
 }
