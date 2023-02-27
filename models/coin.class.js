25a66d2dd3969e45219f1c0753e7c48905cbd57a
Marcel Gregor <marcel_gregor@icloud.com>
2023-02-27 14:50:35 +0100

    refactoring

--- models/coin.class.js
+++ models/coin.class.js
@@ -1,12 +1,10 @@
 class Coin extends DropableObject {
     IMAGES_IDLE =
         [
-
             'assets/img/4. Marcadores/1. Coins/1.png',
             'assets/img/4. Marcadores/1. Coins/2.png',
             'assets/img/4. Marcadores/1. Coins/3.png',
             'assets/img/4. Marcadores/1. Coins/4.png',
-
         ]
 
     constructor(x, y) {
--- models/poison.class.js
+++ models/poison.class.js
@@ -1,15 +1,15 @@
 class Poison extends DropableObject {
-
-    IMAGES_IDLE = [
-        'assets/img/4. Marcadores/Posiขn/Animada/1.png',
-        'assets/img/4. Marcadores/Posiขn/Animada/2.png',
-        'assets/img/4. Marcadores/Posiขn/Animada/3.png',
-        'assets/img/4. Marcadores/Posiขn/Animada/4.png',
-        'assets/img/4. Marcadores/Posiขn/Animada/5.png',
-        'assets/img/4. Marcadores/Posiขn/Animada/6.png',
-        'assets/img/4. Marcadores/Posiขn/Animada/7.png',
-        'assets/img/4. Marcadores/Posiขn/Animada/8.png',
-    ]
+    IMAGES_IDLE =
+        [
+            'assets/img/4. Marcadores/Posiขn/Animada/1.png',
+            'assets/img/4. Marcadores/Posiขn/Animada/2.png',
+            'assets/img/4. Marcadores/Posiขn/Animada/3.png',
+            'assets/img/4. Marcadores/Posiขn/Animada/4.png',
+            'assets/img/4. Marcadores/Posiขn/Animada/5.png',
+            'assets/img/4. Marcadores/Posiขn/Animada/6.png',
+            'assets/img/4. Marcadores/Posiขn/Animada/7.png',
+            'assets/img/4. Marcadores/Posiขn/Animada/8.png',
+        ]
 
     width = 60
     height = 80
